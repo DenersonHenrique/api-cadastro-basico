1ad727765103f860a477e7ae954eb6a3491d39c1
Denin <denersonh@gmail.com>
2019-12-20 10:56:43 -0300

    [master] - Ajuste update.

--- backend/src/routes/produto.router.js
+++ backend/src/routes/produto.router.js
@@ -5,7 +5,7 @@ const controller = require('../controllers/produto.controller');
 router.get('/', controller.get);// Get list items.
 router.post('/', controller.post);// Save Item.
 router.get('/:id', controller.getById);// Get Item by Id.
-router.update('/:id', controller.update);// Update item by id.
+router.put('/:id', controller.update);// Update item by id.
 router.delete('/:id', controller.delete);// Delete item by id.
 
 module.exports = router;
